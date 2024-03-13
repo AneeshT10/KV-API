3b5b1096c4e8e84006a076314ebf24f7e65fdcd6
Aneesh Thippa <aneeshthippa@eduroam-169-233-143-212.ucsc.edu>
2024-03-12 23:09:16 -0700

    pass

--- shard_kvs.py
+++ shard_kvs.py
@@ -20,6 +20,10 @@ message_queue_a = []
 message_queue_d = []
 shard_list = {}
 
+kvaddlock = threading.Lock()
+put_lock = threading.Lock()
+
+
 #broadcasting view to all other replicas
 def put_view(main_url, sa):
     try:
@@ -39,7 +43,7 @@ for addy in view_list:
         try:
             s_url = f'http://{addy}/shard_list'
             print("sending get on", s_url)
-            shard_list = requests.get(s_url, timeout = 0.2).json()["shard_list"]
+            shard_list = requests.get(s_url, timeout = 1).json()["shard_list"]
             break
         except:
             continue
@@ -48,22 +52,12 @@ if shard_count: # represent nodes during startup
      #start up vc_map
     for addy in view_list:
         vc_map[addy] = 0
-    if not shard_list: #Initially, if shard_list is empty
-        for i in range(int(shard_count)):
+    for i in range(int(shard_count)):
             string_id = 's' + str(i+1)
             shard_list[string_id] = []
-        shard_list["s1"].append(sa)
-    else:
+    for addy in view_list:
         insert_shard_key = optimal_insert(shard_list)
-        shard_list[insert_shard_key].append(sa)
-        for addy in view_list:
-            if addy != sa:
-                try:
-                    data = {"isk":insert_shard_key, "sender":sa}
-                    s_url = f'http://{addy}/shard_list'
-                    requests.put(s_url, json = data, timeout = 0.2)
-                except:
-                    continue
+        shard_list[insert_shard_key].append(addy)
 
     
 @app.route("/shard/ids", methods = ['GET'])
@@ -120,6 +114,68 @@ def add_node(ID):
         return {"result": "node added to shard"}, 200
     except:
         return {"error": "some other error"}, 400
+    
+@app.route("/shard/reshard", methods = ['PUT'])
+def reshard():
+    try:
+        new_shard_list = {}
+        new_kv_list = {}
+        data = request.get_json(force = True)
+        reshard_count = data["shard-count"]
+        print(reshard_count)
+        if len(view_list)/reshard_count < 2: # too little nodes to support new shard count
+            return {"error": "Not enough nodes to provide fault tolerance with requested shard count"}, 400
+        #otherwise we reshard
+        #Naive solution
+        for i in range(int(reshard_count)):
+                string_id = 's' + str(i+1)
+                new_shard_list[string_id] = []
+                new_kv_list[string_id] = {}
+        for addy in view_list:
+            insert_shard_key = optimal_insert(new_shard_list)
+            new_shard_list[insert_shard_key].append(addy)
+            kvlist_url = f'http://{addy}/kvlist'
+            try:
+                kvst = requests.get(kvlist_url, timeout=1).json()["kvlist"]
+            except:
+                continue
+            for key in kvst: # list of keys for each shard is assigned
+                key_hash = (int(hashlib.md5(key.encode()).hexdigest(), 16)% int(reshard_count))+1
+                key_shard_assign = 's' + str(key_hash)
+                (new_kv_list[key_shard_assign])[key] = kvst[key]
+        #private API to set all nodes
+        # write newkvlist
+        '''with open('debug.txt', 'w+') as f:
+            f.write("newkvlist" + str(new_kv_list))
+        f.close()'''
+        for current_shard in new_shard_list:
+            for ip in new_shard_list[current_shard]:
+                url = f'http://{ip}/updateshards'
+                send_data = {"shard_list" : new_shard_list, "kvlist" : new_kv_list[current_shard], "shard_count" : reshard_count} 
+                try:
+                    resp = requests.put(url, json = send_data, timeout=1)
+                except:
+                    continue
+        return {"result": "resharded"}, 200
+    except:
+        return {"error" : "reshard not working properly"}, 503
+        
+
+#API to set resharded shardlist and kvlist
+@app.route("/updateshards", methods = ['PUT'])
+def updateshards():
+    global kvlist
+    global shard_list
+    global shard_count
+
+    data = request.get_json(force = True)
+    kvlist = data["kvlist"]
+    shard_list = data["shard_list"]
+    shard_count = data["shard_count"]
+    return {"result" : "done"}, 200
+
+    
+
 
 @app.route("/shard_list", methods = ['GET','PUT'])
 def sh_list():
@@ -133,7 +189,7 @@ def sh_list():
 @app.route("/view", methods = ['GET', 'PUT', 'DELETE'])
 def view():
     if request.method == 'GET':
-        return {"view": view_list, "shard_list" : shard_list, "kvlist": kvlist, "vc_map" : vc_map}, 200
+        return {"view": view_list}, 200 #remember to reset view!!!
     try:
         data = request.get_json(force = True)
     except:
@@ -149,6 +205,7 @@ def view():
     else :
         if data["socket-address"] in view_list:
             view_list.remove(data["socket-address"])
+            del vc_map[data["socket-address"]]
             return {"result": "deleted"}, 200
         else:
             return {"error": "View has no such replica"}, 404
@@ -182,6 +239,7 @@ def kvlist_update(ID):
 #From main replica to receiving replica
 @app.route("/kvmod/<key>", methods = ['PUT', 'DELETE'])
 def kvadd(key):
+    kvaddlock.acquire()
     global vc_map
     if request.method == 'PUT':
         data = request.get_json(force = True)
@@ -196,6 +254,7 @@ def kvadd(key):
         if not deliverable(meta_data, vc_map, sender): #incoming is too far ahead
             #add it to a queue
             message_queue_a.append({"kvpair":(key,val), "causal-metadata" : meta_data, "sender": sender, "key_hash": key_hash})
+            kvaddlock.release()
             return {"result": "done"}, 201
         else: #msg is deliverable
             vc_map = vc_combine(meta_data, vc_map)
@@ -216,6 +275,7 @@ def kvadd(key):
                         vc_map = vc_combine(msg["causal-metadata"], vc_map)
                 for msg in del_msg:
                     message_queue_a.remove(msg)
+            kvaddlock.release()
             return {"result": "done"}, 201
     else:
         data = request.get_json(force = True)
@@ -224,6 +284,7 @@ def kvadd(key):
         if not deliverable(meta_data, vc_map, sender): #incoming is too far ahead
             #add it to a queue
             message_queue_d.append({"key":key, "causal-metadata" : meta_data, "sender": sender, "key_hash": key_hash})
+            kvaddlock.release()
             return {"result": "done"}, 201
         else: #msg is deliverable
             vc_map = vc_combine(meta_data, vc_map)
@@ -244,12 +305,15 @@ def kvadd(key):
                         vc_map = vc_combine(msg["causal-metadata"], vc_map)
                 for msg in del_msg:
                     message_queue_d.remove(msg)
+            kvaddlock.release()
             return {"result": "done"}, 201
 
 #Key value operations
 @app.route('/kvs', strict_slashes=False, defaults={'key': ''}, methods=['GET', 'PUT', 'DELETE'])
 @app.route("/kvs/<key>", methods = ['GET', 'PUT', 'DELETE'])
 def kvs(key):
+    if request.method != 'GET':
+        put_lock.acquire()
     if request.method == 'PUT':
         try:
             data = request.get_json(force = True)
@@ -264,20 +328,25 @@ def kvs(key):
                     try:
                         forward_url = f'http://{ip}/kvs/{key}'
                         response = requests.put(forward_url, json = data, timeout=1)
+                        put_lock.release()
                         return response.json(), response.status_code
                     except:
                         continue
+                put_lock.release()
                 return {"error" : "no replica to forward to"}, 500
 
             meta_data = data["causal-metadata"]
             val = data["value"]
             if 'value' not in data.keys():
+                put_lock.release()
                 return {"error": "PUT request does not specify a value"}, 400
             elif len(key) > 50:
+                put_lock.release()
                 return {"error": "Key is too long"}, 400
             if meta_data is not None:
                 if meta_data != vc_map:
-                    return {"error": "Causal dependencies not satisfied; try again later", "message_queue": message_queue_a }, 503
+                    put_lock.release()
+                    return {"error": "Causal dependencies not satisfied; try again later"}, 503
             vc_map[sa] += 1 #Message about to be sent
             meta_data = vc_map #Updating metadata to match current main replica vector clock
             #broadcast replace to every other replica
@@ -303,18 +372,21 @@ def kvs(key):
                 for i in ad_rm:
                     try:
                         view_list.remove(i)
+                        del vc_map[i]
                     except:
                         continue
+                put_lock.release()
             t3 = threading.Thread(target=put_kvs_broadcast)
             t3.start()
             if key in kvlist.keys():
                 kvlist[key] = data["value"]
-                return {"result": "replaced", "causal-metadata": meta_data}, 200
+                return {"result": "replaced", "causal-metadata": meta_data, "shard-id" : key_shard_assign}, 200
             else:
                 kvlist[key] = data["value"]
-                return {"result" : "created", "causal-metadata": meta_data, "vc_map" :vc_map, "sa" :sa}, 201
+                return {"result" : "created", "causal-metadata": meta_data, "shard-id" : key_shard_assign}, 201
         except: 
-            #return {"error": "PUT request does not specify a value"}, 405
+            put_lock.release()
+            return {"error": "PUT request does not specify a value"}, 405
     if request.method == 'DELETE':
         try:
             data = request.get_json(force = True)
@@ -329,6 +401,7 @@ def kvs(key):
                     try:
                         forward_url = f'http://{ip}/kvs/{key}'
                         response = requests.delete(forward_url, json = data, timeout=1)
+                        put_lock.release()
                         return response.json(), response.status_code
                     except:
                         continue
@@ -336,8 +409,10 @@ def kvs(key):
             meta_data = data["causal-metadata"]
             if meta_data is not None:
                 if meta_data != vc_map:
+                    put_lock.release()
                     return {"error": "Causal dependencies not satisfied; try again later"}, 503
             if key not in kvlist.keys():
+                put_lock.release()
                 return {"error": "Key does not exist"}, 404
             vc_map[sa] += 1 #Message about to be sent
             meta_data = vc_map #Updating metadata to match current main replica vector clock
@@ -364,15 +439,18 @@ def kvs(key):
                                     continue
                 for i in ad_rm:
                     try:
+                        del vc_map[i]
                         view_list.remove(i)
                     except:
                         continue
+                put_lock.release()
             t2 = threading.Thread(target=delete_kvs_broadcast)
             t2.start()
             del kvlist[key]
-            return {"result": "deleted", "causal-metadata": meta_data}, 200
-        except: 
-            #return {"error": "Invalid argument"}, 405
+            return {"result": "deleted", "causal-metadata": meta_data, "shard-id" : key_shard_assign}, 200
+        except:
+            put_lock.release()
+            return {"error": "Invalid argument"}, 405
     else: # request is GET
         try:
             data = request.get_json(force = True)
@@ -393,11 +471,19 @@ def kvs(key):
             meta_data = data["causal-metadata"]
             if meta_data is not None:
                 if meta_data != vc_map:
-                    return {"error": "Causal dependencies not satisfied; try again later"}, 503
+                    set1 = set(meta_data.items())
+                    set2 = set(vc_map.items())
+                    difference = set1 - set2
+                    d = list(difference)
+                    count = 0
+                    for tup in d:
+                        count += tup[1]
+                    if count != 0:
+                        return {"error": "Causal dependencies not satisfied; try again later"}, 503
             else:
                 meta_data = vc_map
             if key in kvlist.keys():
-                return {"result": "found", "value": kvlist[key], "causal-metadata": meta_data}, 200
+                return {"result": "found", "value": kvlist[key], "causal-metadata": meta_data, "shard-id" : key_shard_assign}, 200
             else: 
                 return {"error": "Key does not exist"}, 404
         except:
